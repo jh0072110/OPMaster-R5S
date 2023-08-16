75837e305ff159bfd5c299312076bd9fe9cc162f
BigBB2110 <50496854+jh0072110@users.noreply.github.com>
2023-08-16 11:07:39 -0500

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,8 +16,10 @@ rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-vssr
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-alist
 rm -rf feeds/luci/themes/luci-theme-argon
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/psw
