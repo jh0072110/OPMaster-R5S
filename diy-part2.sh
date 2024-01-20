c48c19711b4a256fb743146f195f44db00708d89
BigBB2110 <50496854+jh0072110@users.noreply.github.com>
2024-01-20 12:48:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,5 +22,4 @@ git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 git clone -b main https://github.com/xiaorouji/openwrt-passwall package/psw
-git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
