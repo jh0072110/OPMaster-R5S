3791354ad2e244898fc4b14e69c270f69b9bcc36
BigBB2110 <50496854+jh0072110@users.noreply.github.com>
2024-05-10 02:25:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,7 +19,7 @@ rm -rf feeds/luci/applications/luci-app-homeproxy
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/packages/net/alist
 rm -rf feeds/packages/net/mosdns
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.20.30.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
